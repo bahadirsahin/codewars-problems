8e3d2c41e413caa55a3aeb110e6be61778fcafc6
bahadirsahin <eyupbahadirsahin@gmail.com>
2023-07-30 15:39:01 +0300

    added q156

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ import (
 
 func main() {
 	spew.Dump(
+		set6.Q156(12),
 		set6.Q156(513),
+		set6.Q156(2017),
+		set6.Q156(414),
+		set6.Q156(144),
+		set6.Q156(444),
+		set6.Q156(9876543210),
 	)
 
 	// spew.Dump(
@@ -17,5 +23,9 @@ func main() {
 
 	// spew.Dump(
 	// 	set6.Q156(531),
+	// )
+
+	// spew.Dump(
+	// 	set6.Q156(21581957621),
 	// )
 }
--- set6/q156.go
+++ set6/q156.go
@@ -1,57 +1,104 @@
 package set6
 
 import (
-	"math"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 func Q156(n int) int {
-	if n == 0 {
+	nn := n
+
+	if nn == 0 {
 		return 0
 	}
 
-	nn := n
-	digits := []int{}
-	perms := []int{}
-	m := map[int]bool{}
-
-	for ; nn > 0; nn /= 10 {
-		digits = append(digits, nn%10)
-	}
-
-	Q156Rec(digits, 0, len(digits)-1, &perms, &m)
-	sort.Ints(perms)
-
-	if n == perms[len(perms)-1] {
+	if nn/10 == 0 {
 		return -1
 	}
 
-	for i, v := range perms {
-		if v == n {
-			return perms[i+1]
+	m := map[int]bool{}
+
+	for ; nn > 0; nn /= 10 {
+		d := nn % 10
+
+		if !m[d] {
+			m[d] = true
 		}
 	}
 
-	return 0
-}
+	if len(m) == 1 {
+		return -1
+	}
 
-func Q156Rec(digits []int, l, r int, perms *[]int, m *map[int]bool) {
-	if l == r {
-		n := 0
+	str := strconv.Itoa(n)
 
-		for i, d := range digits {
-			n += (d * int(math.Pow10(len(digits)-1-i)))
-		}
+	allDesc := true
 
-		if !(*m)[n] {
-			*perms = append(*perms, n)
-			(*m)[n] = true
+	for i := 0; i < len(str)-1; i++ {
+		ld, _ := strconv.Atoi(string(str[i]))
+		rd, _ := strconv.Atoi(string(str[i+1]))
+
+		if rd > ld {
+			allDesc = false
 		}
 	}
 
-	for i := l; i <= r; i++ {
-		digits[i], digits[l] = digits[l], digits[i]
-		Q156Rec(digits, l+1, r, perms, m)
-		digits[i], digits[l] = digits[l], digits[i]
+	if allDesc {
+		return -1
 	}
+
+	res, pivot, pivotIdx, pivotSub := 0, 0, 0, 0
+
+	for i := len(str) - 1; i > 0; i-- {
+		l, r := 0, 0
+		l, _ = strconv.Atoi(string(str[i-1]))
+		r, _ = strconv.Atoi(string(str[i]))
+
+		if l < r {
+			pivot = l
+			pivotIdx = i - 1
+
+			break
+		}
+	}
+
+	rightPart := str[pivotIdx+1:]
+	rightPartArr := strings.Split(rightPart, "")
+	rightPartIntArr := []int{}
+
+	for _, v := range rightPartArr {
+		vd, _ := strconv.Atoi(v)
+
+		rightPartIntArr = append(rightPartIntArr, vd)
+	}
+
+	sort.Ints(rightPartIntArr)
+
+	for _, v := range rightPartIntArr {
+		if v > pivot {
+			pivotSub = v
+
+			break
+		}
+	}
+
+	for i, v := range rightPartIntArr {
+		if v == pivotSub {
+			rightPartIntArr[i] = pivot
+
+			break
+		}
+	}
+
+	resStr := str[:pivotIdx]
+	resStr += strconv.Itoa(pivotSub)
+
+	for _, v := range rightPartIntArr {
+		resStr += strconv.Itoa(v)
+	}
+
+	res, _ = strconv.Atoi(resStr)
+
+	return res
 }
