b6e376aa075471f02bdbe138de1d017973ae96b3
bahadirsahin <eyupbahadirsahin@gmail.com>
2023-07-30 17:33:07 +0300

    updated q157

--- set6/q157.go
+++ set6/q157.go
@@ -17,22 +17,34 @@ func Q157(intervals [][2]int) int {
 		return intervals[i][0] < intervals[j][0]
 	})
 
-	sum := 0
-	x := intervals[0][0]
+	arr := [][2]int{intervals[0]}
 
-	for _, i := range intervals {
-		if i[1] >= x {
+	for i, in := range intervals {
+		if i == 0 {
+			continue
+		}
+
+		start, end, lastEnd := in[0], in[1], arr[len(arr)-1][1]
+
+		if start <= lastEnd {
 			max := 0
 
-			if x > i[0] {
-				max = x
+			if end > lastEnd {
+				max = end
 			} else {
-				max = i[0]
+				max = lastEnd
 			}
 
-			sum += i[1] - max
-			x = i[1]
+			arr[len(arr)-1][1] = max
+		} else {
+			arr = append(arr, in)
 		}
+	}
+
+	sum := 0
+
+	for _, in := range arr {
+		sum += (in[1] - in[0])
 	}
 
 	return sum
